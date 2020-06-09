83c95b62f7ad1167135ad0b86d927ef5d336867e
RashmiKalpeshChauhan <53113574+RashmiKalpeshChauhan@users.noreply.github.com>
2020-06-09 18:47:56 +0400

    Update searchFlight.js

--- cypress/integration/tests/searchFlight.js
+++ cypress/integration/tests/searchFlight.js
@@ -2,7 +2,7 @@
 require('cypress-xpath')
 describe('Login Test case', () => {
     beforeEach(() => {
-       // cy.fixture('testData.json').as('testdata')  
+        cy.fixture('testData.json').as('testdata')  
        // cy.visit("https://www.tajawal.ae/en")   
        cy.visit("https://ae.almosafer.com/ar")        
       })    
@@ -18,37 +18,16 @@ describe('Login Test case', () => {
                 expect(priceValue).to.be.lessThan(10000);
             })
         })
+        
     })
 
   
 
-    it("ticket value should less than 10000", () => {
-        cy.get("[data-testid=Header__FlightsNavigationTab]").click();
-        cy.get("[data-testid='FlightSearchBox__FromAirportInput']").type("DXB");
-        cy.get("span:contains('DXB')").click({ force: true })
-        cy.get("[data-testid='FlightSearchBox__ToAirportInput']").type("DEL");
-        cy.get("span:contains('DEL')").click({ force: true })
-        cy.wait(50)
-         cy.xpath("//input[@data-testid='FlightSearchBox__FareCalendarCheckbox']").click({force: true})
-        cy.get("[data-testid='FlightSearchBox__FromDateButton']").click()
-        const date = Cypress.moment().get('date') + 5;
-        if (date > 30) {
-            date = 30;
-        }
-        cy.get('div[class="DayPicker-Months"]')
-            .find('span', date)
-            .contains(date)
-            .click();
-        cy.get('[data-testid=FlightSearchBox__RemoveReturnButton]').click()
-        cy.wait(50)
-        cy.get("[data-testid='FlightSearchBox__SearchButton']").first().click()
-        cy.xpath("//td/div/span[2]").each($el => {
-            const price = $el.text();
-            const priceRegex = price.replace(/[$.,]+/g, '');
-            const priceValue = parseInt(priceRegex)
-            cy.log('priceValue: ', priceValue);
-            expect(priceValue).to.be.lessThan(10000);
-        })
+    it("ticket value should less than 10000", (testData) => {        
+        cy.searchFlight(origin,destination);    
+        cy.selectDate();        
+        cy.searchButton();
+        cy.lowestPrice();        
     })
     it("validate the languge of page is english", () => {
         if(cy.title().should('not.have.value', 'tajawal')){
